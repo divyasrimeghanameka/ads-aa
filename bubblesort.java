9c0998a383818c3f36e57dceb3f335b324bd1ef0
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-12-14 15:35:48 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class BubbleSort
+{
+	void bubbleSort(int arr[])
+	{
+		int n=arr.length;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				
+				}
+			}
+		}
+	}
+	public static void main(String[]args)
+	{
+		BubbleSort obj=new BubbleSort();
+		int a[]={62,52,12,01,98,67,36,42};
+		obj.bubbleSort(a);
+		int n=a.length;
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]+" ");
+		}
+		System.out.println();
+	}
+}
