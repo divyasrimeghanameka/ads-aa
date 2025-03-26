83abe3883812a820c3413e43b25fcef283a504e1
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2025-03-26 11:36:08 +0530

    Update knapsack using dp.java

--- knapsack using dp.java
+++ knapsack using dp.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-
-public class Knapsack{
-    public static void main(String[] args) {
+public class Knapsack
+{
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of Objects:");
         int n = sc.nextInt();
@@ -10,41 +11,53 @@ public class Knapsack{
         int[] w = new int[n];
         
         System.out.println("Enter profits for each object:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             p[i] = sc.nextInt();
         }
         
         System.out.println("Enter weights for each object:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             w[i] = sc.nextInt();
         }
         
         System.out.println("Enter maximum weight of knapsack:");
         int m = sc.nextInt();
-        
         knapsack(m, w, p, n);
-    }
-
-    public static void knapsack(int m, int[] w, int[] p, int n) {
+   }
+   public static void knapsack(int m, int[] w, int[] p, int n) 
+   {
         int[][] k = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
+        for (int i = 0; i <= n; i++) 
+        {
+            for (int j = 0; j <= m; j++) 
+            {
+                if (i == 0 || j == 0) 
+                {
                     k[i][j] = 0;
-                } else if (w[i - 1] <= j) {
+                } 
+                else if (w[i - 1] <= j) 
+                {
                     k[i][j] = Math.max(k[i - 1][j], k[i - 1][j - w[i - 1]] + p[i - 1]);
-                } else {
+                } 
+                else 
+                {
                     k[i][j] = k[i - 1][j];
                 }
             }
         }
         int[] ar=new int[n];
         int i = n, j = m;
-        while (i > 0 && j > 0) {
-            if (k[i][j] == k[i - 1][j]) {
+        while (i > 0 && j > 0) 
+        {
+            if (k[i][j] == k[i - 1][j]) 
+            {
                 ar[i-1]=0;
                 i--;
-            } else {
+            } 
+            else 
+            {
                 ar[i-1]=1;
                 i--;
                 j -= w[i];
@@ -52,12 +65,12 @@ public class Knapsack{
         }
 
         System.out.println("Maximum profit: " + k[n][m]);
-        for(int num:ar){
+        for(int num:ar)
+        {
             System.out.print(num+" ");
         }
     }
 }
-
 
 
 
